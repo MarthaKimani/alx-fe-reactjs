c6192a0325858705e8c394f8e849fb3d3bccf929
MarthaKimani <marthakimani423@gmail.com>
2025-09-06 11:59:24 +0300

    Added some files

---
+++ MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <main style={{ padding: "20px", backgroundColor: "#e6f2ff", minHeight: "200px" }}>
+      <h2 style={{ color: "darkslateblue" }}>Welcome to My Profile</h2>
+      <p style={{ fontSize: "16px", lineHeight: "1.6" }}>
+        I love exploring different cities and sharing my experiences with friends!
+      </p>
+    </main>
+  );
+}
+export default MainContent;
