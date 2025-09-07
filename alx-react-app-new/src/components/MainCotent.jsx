a0428f8f04fa0857a31fee3da5393b427f417fbc
MarthaKimani <marthakimani423@gmail.com>
2025-09-07 15:09:40 +0300

    adjusted my code

---
+++ alx-react-app-new/src/components/MainCotent.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function MainContent() {
+  return (
+    <main style={{ padding: '20px', backgroundColor: '#eef2f7', minHeight: '300px' }}>
+      <h2 style={{ color: 'darkslategray', textAlign: 'center' }}>Welcome to My Page</h2>
+      <p style={{ textAlign: 'center', fontSize: '18px', marginTop: '10px' }}>
+        Explore some of my favorite cities and learn more about me!
+      </p>
+    </main>
+  );
+}
+
+export default MainContent;
