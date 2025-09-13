7cb7a0973e267ca131d5af0e88994abaf5c0d1f7
MarthaKimani <marthakimani423@gmail.com>
2025-09-13 12:14:25 +0300

    corrected an issue

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,11 +1,12 @@
 // src/components/RecipeDetails.jsx
 import { useParams } from "react-router-dom";
-import { getRecipes } from "./recipeStore";
+import { useRecipeStore } from "./recipeStore";
 
 const RecipeDetails = () => {
-  const { id } = useParams(); // grab recipe id from the route
-  const recipes = getRecipes();
-  const recipe = recipes.find(r => r.id === Number(id));
+  const { id } = useParams(); // get recipe ID from URL
+  const recipe = useRecipeStore((state) =>
+    state.recipes.find((r) => r.id === Number(id))
+  );
 
   if (!recipe) {
     return <p>Recipe not found</p>;
