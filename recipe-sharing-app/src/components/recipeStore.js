738e9f9d9ac8925f12a31c5a47a1c96d0cbf0783
MarthaKimani <marthakimani423@gmail.com>
2025-09-13 12:02:32 +0300

    modified a file

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,12 +1,34 @@
-import { create } from 'zustand'
+// src/components/recipeStore.js
 
-const useRecipeStore = create((set) => ({
-  recipes: [],
-  addRecipe: (newRecipe) =>
-    set((state) => ({
-      recipes: [...state.recipes, newRecipe],
-    })),
-  setRecipes: (recipes) => set({ recipes }),
-}))
+let recipes = [];
 
-export default useRecipeStore
+// Update a recipe by id
+export function updateRecipe(id, updatedData) {
+  const index = recipes.findIndex(recipe => recipe.id === id);
+  if (index !== -1) {
+    recipes[index] = { ...recipes[index], ...updatedData };
+    return recipes[index];
+  }
+  return null;
+}
+
+// Delete a recipe by id
+export function deleteRecipe(id) {
+  const index = recipes.findIndex(recipe => recipe.id === id);
+  if (index !== -1) {
+    const deleted = recipes.splice(index, 1);
+    return deleted[0];
+  }
+  return null;
+}
+
+// Optional helpers
+export function addRecipe(recipe) {
+  recipes.push(recipe);
+  return recipe;
+}
+
+export function getRecipes() {
+  return recipes;
+}
+
