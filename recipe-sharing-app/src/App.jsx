4a866eed216acfbbfedc12abaeb8ff69a08d2626
MarthaKimani <marthakimani423@gmail.com>
2025-09-13 12:30:32 +0300

    modified some files

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,45 +1,19 @@
-// src/App.jsx
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import AddRecipeForm from './components/AddRecipeForm'
-import RecipeList from './components/RecipeList'
-import RecipeDetails from './components/RecipeDetails'
-import EditRecipeForm from './components/EditRecipeForm'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+import AddRecipeForm from "./components/AddRecipeForm";
 
 function App() {
   return (
     <Router>
-      <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-        <h1>🍳 Recipe Sharing App</h1>
-
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <AddRecipeForm />
-                <RecipeList />
-              </>
-            }
-          />
-
-          <Route path="/recipes/:id" element={<RecipeDetails />} />
-          <Route path="/edit/:id" element={<EditRecipeForm />} />
-
-          {/* fallback route */}
-          <Route
-            path="*"
-            element={
-              <div style={{ padding: 20 }}>
-                <h2>Page not found</h2>
-              </div>
-            }
-          />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<RecipeList />} />
+        <Route path="/recipe/:id" element={<RecipeDetails />} />
+        <Route path="/add" element={<AddRecipeForm />} />
+      </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
 
