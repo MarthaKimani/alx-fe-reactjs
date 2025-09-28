fa419c6181b0c25f3972709b92013bad93ecfa28
MarthaKimani <marthakimani423@gmail.com>
2025-09-28 11:40:18 +0300

    modified App.jsx

--- tailwind-react-integration/src/App.jsx
+++ tailwind-react-integration/src/App.jsx
@@ -1,12 +1,9 @@
+import UserProfile from "./components/UserProfile";
+
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">
-        Tailwind is working 🎉
-      </h1>
-      <p className="mt-4 text-lg text-gray-700">
-        You can now use Tailwind CSS classes in your React project.
-      </p>
+    <div>
+      <UserProfile />
     </div>
   );
 }
