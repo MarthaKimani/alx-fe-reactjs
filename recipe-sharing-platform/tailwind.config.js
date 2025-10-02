ca8cffe2ee4b2b0a2f0be490c1c4027c2f73a7ab
MarthaKimani <marthakimani423@gmail.com>
2025-10-02 12:37:58 +0300

    fixed tailwind.config.js

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",                     // Vite root index.html
-    "./src/**/*.{js,ts,jsx,tsx}",       // All React source files
+    "./public/index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
