a6f9a49df5bef415bff7935c644370bc49f6d1ec
MarthaKimani <marthakimani423@gmail.com>
2025-10-02 13:06:10 +0300

    modified some files

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,18 +1,16 @@
-
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// src/App.jsx
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import RecipeDetail from "./components/RecipeDetail";
 
 function App() {
   return (
     <Router>
-      <div className="container mx-auto p-6">
-        <h1 className="text-3xl font-bold text-center mb-8">Recipe Sharing Platform</h1>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/recipe/:id" element={<RecipeDetail />} />
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
     </Router>
   );
 }
