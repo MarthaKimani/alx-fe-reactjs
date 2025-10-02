1237dbb906c47e7f04b3e3d234b2b1401f5c438f
MarthaKimani <marthakimani423@gmail.com>
2025-10-02 12:27:03 +0300

    added new folder recipe-sharing-platform

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",                     // Vite serves from index.html
+    "./src/**/*.{js,ts,jsx,tsx}",       // All source files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
